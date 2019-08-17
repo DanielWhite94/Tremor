d5e48d151678b2da803cf135f39871c03a12f3dd
DJW <dan@fake.com>
2019-08-17 03:03:37 +0100

    Made Colour mul function better in case of factor>1

--- engine/colour.cpp
+++ engine/colour.cpp
@@ -1,7 +1,19 @@
+#include <assert.h>
+
 #include "colour.h"
 
 void Colour::mul(double factor) {
-	r*=factor;
-	g*=factor;
-	b*=factor;
+	assert(factor>=0.0);
+	if (r*factor>255.0)
+		r=255;
+	else
+		r*=factor;
+	if (g*factor>255.0)
+		g=255;
+	else
+		g*=factor;
+	if (b*factor>255.0)
+		b=255;
+	else
+		b*=factor;
 }
