fa8a3ffbd53e54e21fbb7837cf1330bdf46093d5
DJW <dan@fake.com>
2019-08-20 18:51:13 +0100

    File version of Map constructor now parses width/height and loads textures.

--- engine/src/map.cpp
+++ engine/src/map.cpp
@@ -43,10 +43,79 @@ namespace TremorEngine {
 
 		// Allocate textures vector
 		textures=new std::vector<Texture *>;
-		// TODO: this
+
+		// Set fields to indicate empty map initially
 		width=0;
 		height=0;
 		blocks=NULL;
+
+		// Load map as JSON object
+		// TODO: check for failure
+		std::ifstream mapStream(file);
+		json jsonRoot;
+		mapStream >> jsonRoot;
+
+		// Parse JSON data - root 'map' object
+		json jsonMap=jsonRoot["map"];
+		if (!jsonMap.is_object()) {
+			std::cout << "Could not load map: no root map object." << std::endl;
+			return;
+		}
+
+		if (!jsonMap["width"].is_number() || !jsonMap["height"].is_number()) {
+			std::cout << "Could not load map: missing/bad 'width' or 'height' values in root map object." << std::endl;
+			return;
+		}
+		width=jsonMap["width"].get<int>();
+		height=jsonMap["height"].get<int>();
+
+		if (width<1 || height<1) {
+			std::cout << "Could not load map: bad 'width' or 'height' values (width=" << width << ", height=" << height << ")." << std::endl;
+			return;
+		}
+
+		// Allocate blocks array
+		blocks=(Block *)malloc(sizeof(Block)*width*height);
+		if (blocks==NULL) {
+			std::cout << "Could not load map: could not allocate blocks array." << std::endl;
+			return;
+		}
+
+		// Fill blocks array with height=0 to imply empty
+		for(unsigned i=0; i<width*height; ++i)
+			blocks[i].height=0.0;
+
+		// Parse JSON data - load textures
+		json jsonTextures=jsonMap["textures"];
+		if (jsonTextures.is_array()) {
+			for(auto &entry : jsonTextures.items()) {
+				json jsonTexture=entry.value();
+				if (!jsonTexture["id"].is_number() || !jsonTexture["file"].is_string()) {
+					std::cout << "Warning while loading map: bad texture '" << jsonTexture << "'." << std::endl;
+					continue;
+				}
+
+				int textureId=jsonTexture["id"].get<int>();
+				if (textureId<0) {
+					std::cout << "Warning while loading map: bad texture id " << jsonTexture["id"] << "." << std::endl;
+					continue;
+				}
+				std::string textureFile=jsonTexture["file"].get<std::string>();
+				if (!addTexture(textureId, textureFile.c_str())) {
+					std::cout << "Warning while loading map: could not load texture " << textureId << " at '" << textureFile << "'." << std::endl;
+					continue;
+				}
+			}
+		}
+
+		// Parse JSON data - load blocks
+		// TODO: this
+
+		// Parse JSON data - load objects
+		// TODO: this
+
+		// Done
+		hasInit=true;
 	}
 
 	Map::~Map() {
