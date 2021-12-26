c33b11f5cfb092b816e9e86605cd6b46a9abf4eb
danielwhite94 <danielwhite94@gmail.com>
2021-12-26 21:09:50 +0000

    Added various preset colours

--- engine/src/colour.cpp
+++ engine/src/colour.cpp
@@ -2,6 +2,13 @@
 
 #include "colour.h"
 
+const Colour colourBlack   ={.r=  0, .g=  0, .b=  0, .a=255};
+const Colour colourWhite   ={.r=255, .g=255, .b=255, .a=255};
+const Colour colourGrey    ={.r=196, .g=196, .b=196, .a=255};
+const Colour colourRed     ={.r=255, .g=  0, .b=  0, .a=255};
+const Colour colourGreen   ={.r=  0, .g=255, .b=  0, .a=255};
+const Colour colourBlue    ={.r=  0, .g=  0, .b=255, .a=255};
+
 void Colour::mul(double factor) {
 	assert(factor>=0.0);
 	if (r*factor>255.0)
--- engine/src/colour.h
+++ engine/src/colour.h
@@ -10,4 +10,11 @@ public:
 	void mul(double factor); // does not affect alpha
 };
 
+extern const Colour colourBlack;
+extern const Colour colourWhite;
+extern const Colour colourGrey;
+extern const Colour colourRed;
+extern const Colour colourGreen;
+extern const Colour colourBlue;
+
 #endif
